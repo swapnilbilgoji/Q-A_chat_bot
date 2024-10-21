128bfdb6cd967c71c710fe4700b66931648d2d82
Swapnil Bilgoji <swapnilbilgoji@gmail.com>
2024-10-21 19:39:35 +0530

    change

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## function to load Gemini Pro model and get repsonses
 model=genai.GenerativeModel("gemini-pro") 
-chat = model.start_chat(history=[])
+chat = model.start_chat(history=[])   
 def get_gemini_response(question):
     
     response=chat.send_message(question,stream=True)
